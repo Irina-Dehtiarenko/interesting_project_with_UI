ace25d1c095804ed14e9586555a4074196de180a
Irina-Dehtiarenko <93826301+Irina-Dehtiarenko@users.noreply.github.com>
2023-01-06 17:49:06 +0100

    implemented page navigation with event delegation

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function (e) {
   e.preventDefault();
@@ -30,104 +32,8 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// /////////////////
-// notes
-// /////////////////
-// SELECTED ELEMENTS
-// console.log(document.documentElement); //all HTML document
-// console.log(document.head);
-// console.log(document.body);
-const allSelections = document.querySelectorAll('.section');
-const header = document.querySelector('.header');
-document.getElementById('section--1');
-const allButtons = document.getElementsByTagName('button');
-
-// console.log(allButtons); //HTMLCollection(9) - like an array[button.btn--text.btn--scroll-to, button.btn.operations__tab.operations__tab--1.operations__tab--active]
-//na żywo onawia się informacja
-
-document.getElementsByClassName('btn'); //HTMLCollection(9)
-
-// /////////////////
-// Creating and inaerting elements
-// .insertAdjacentHTML
-
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-// message.textContent = 'We use cookies for improved functionality and analytics'
-message.innerHTML =
-  'We use cookies for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it!</button>  ';
-header.prepend(message);
-// header.append(message);
-// header.append(message.cloneNode(true));
-
-// header.before(message);
-// header.after(message);
-
-/////////////////
-// Delete element
-
-document.querySelector('.btn--close-cookie').addEventListener('click', () => {
-  message.remove();
-  // the old way for do the same:
-  // message.parentElement.removeChild(message)
-});
-
-/////////////////
-
-// Styles, atributes and classes
-// Styles
-message.style.backgroundColor = '#37383d';
-message.style.width = '120%'; // = inline styles in HTML
-
-// console.log(message.style.backgroundColor); // rgb(55, 56, 61)
-// działa tylko dla stylów name zaimplementowanych liniowo w HTML
-
-// console.log(getComputedStyle(message)); //uzyskamy wszystkie style
-// console.log(getComputedStyle(message).color);
-// console.log(getComputedStyle(message).height);
-
-message.style.height =
-  Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
-
-// document.documentElement.style.setProperty('--color-primary', 'orangered'); //zmieniamy zmienne CSS
-
-// Attributes
-const logo = document.querySelector('.nav__logo');
-// console.log(logo.alt); //Bankist logo
-// console.log(logo.src); //http://127.0.0.1:8080/img/logo.png
-// console.log(logo.className);
-
-logo.alt = 'Beautiful minimalist logo'; //changing slt atribute
-
-// // Non-standart attribute
-// console.log(logo.designer); //doesn't work
-// console.log(logo.getAttribute('designer')); //work
-// logo.setAttribute('company', 'Bankist');
-
-// console.log(logo.src); //http://127.0.0.1:8080/img/logo.png - absolute
-// console.log(logo.getAttribute('src')); //img/logo.png - relative, żywy url, zależny od lokalizacji foldera
-
-// const link = document.querySelector('.nav__link--btn');
-// console.log(link.href); //http://127.0.0.1:8080/#
-// console.log(link.getAttribute('href')); //#
-
-// Data attributes
-// console.log(logo.dataset.versionNumber); //3.0
-
-// Clases
-
-// logo.classList.add('c', 'j');
-// logo.classList.remove('c', 'l');
-// logo.classList.toggle('c');
-// logo.classList.contains('c'); //not includes
-
-// don't use
-// logo.className = 'jonas'; //zastąpi wszystkie klassy
-
 ///////////////////////////////////
-
-const btnScrollTo = document.querySelector('.btn--scroll-to');
-const section1 = document.querySelector('#section--1');
+// Scroling
 
 btnScrollTo.addEventListener('click', e => {
   const s1coords = section1.getBoundingClientRect();
@@ -161,6 +67,135 @@ btnScrollTo.addEventListener('click', e => {
   });
 });
 
+///////////////////////////////////
+// Page navigation:
+
+// Mało wydajne rozwiązanie:
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+
+//     const id = this.getAttribute('href');
+//     console.log(id);
+
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   });
+// });
+
+// Event delegation:
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  // console.log(e.target);
+
+  // Matching strategy
+  if (e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+
+    const id = e.target.getAttribute('href');
+    console.log(id);
+
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+
+// /////////////////
+// notes
+// ///////////////////////////////////////////////////////////////////////
+// SELECTED ELEMENTS
+// console.log(document.documentElement); //all HTML document
+// console.log(document.head);
+// console.log(document.body);
+// const allSelections = document.querySelectorAll('.section');
+// const header = document.querySelector('.header');
+// document.getElementById('section--1');
+// const allButtons = document.getElementsByTagName('button');
+
+// console.log(allButtons); //HTMLCollection(9) - like an array[button.btn--text.btn--scroll-to, button.btn.operations__tab.operations__tab--1.operations__tab--active]
+//na żywo onawia się informacja
+
+// document.getElementsByClassName('btn'); //HTMLCollection(9)
+
+// /////////////////
+// Creating and inaerting elements
+// .insertAdjacentHTML
+
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.textContent = 'We use cookies for improved functionality and analytics'
+// message.innerHTML =
+//   'We use cookies for improved functionality and analytics. <button class = "btn btn--close-cookie">Got it!</button>  ';
+// header.prepend(message);
+// header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+/////////////////
+// Delete element
+
+// document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+//   message.remove();
+//   // the old way for do the same:
+//   // message.parentElement.removeChild(message)
+// });
+
+/////////////////
+
+// Styles, atributes and classes
+// Styles
+// message.style/width = '120%'; // = inline styles in HTML
+
+// console.log(message.style.backgroundColor); // rgb(55, 56, 61)
+// działa tylko dla stylów name zaimplementowanych liniowo w HTML
+
+// console.log(getComputedStyle(message)); //uzyskamy wszystkie style
+// console.log(getComputedStyle(message).color);
+// console.log(getComputedStyle(message).height);
+
+// message.style.height =
+//   Number.parseFloat(getComputedStyle(message).height, 10) + 30 + 'px';
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered'); //zmieniamy zmienne CSS
+
+// Attributes
+// const logo = document.querySelector('.nav__logo');
+// console.log(logo.alt); //Bankist logo
+// console.log(logo.src); //http://127.0.0.1:8080/img/logo.png
+// console.log(logo.className);
+
+// logo.alt = 'Beautiful minimalist logo'; //changing slt atribute
+
+// // Non-standart attribute
+// console.log(logo.designer); //doesn't work
+// console.log(logo.getAttribute('designer')); //work
+// logo.setAttribute('company', 'Bankist');
+
+// console.log(logo.src); //http://127.0.0.1:8080/img/logo.png - absolute
+// console.log(logo.getAttribute('src')); //img/logo.png - relative, żywy url, zależny od lokalizacji foldera
+
+// const link = document.querySelector('.nav__link--btn');
+// console.log(link.href); //http://127.0.0.1:8080/#
+// console.log(link.getAttribute('href')); //#
+
+// Data attributes
+// console.log(logo.dataset.versionNumber); //3.0
+
+// Clases
+
+// logo.classList.add('c', 'j');
+// logo.classList.remove('c', 'l');
+// logo.classList.toggle('c');
+// logo.classList.contains('c'); //not includes
+
+// don't use
+// logo.className = 'jonas'; //zastąpi wszystkie klassy
+
+///////////////////////////////////
+
 // // Remove
 // const h1 = document.querySelector('h1');
 
@@ -183,36 +218,36 @@ btnScrollTo.addEventListener('click', e => {
 
 ////////////////////////
 // Event propagation
-// rgb(255,255,255)
-const randomInt = (min, max) =>
-  Math.floor(Math.random() * (max - min + 1) + min);
-const randomColor = () =>
-  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
-console.log(randomColor());
+// // rgb(255,255,255)
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+// console.log(randomColor());
 
-document.querySelector('.nav__link').addEventListener('click', function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log('LINK', e.target, e.currentTarget);
-  // e.target - gdzie event się wydaży - nav__link
-  //e.currentTarget - element którego dotyczy obecne wydarzenie(albo ten, albo rodzic)
-  console.log(e.currentTarget === this); //true
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('LINK', e.target, e.currentTarget);
+//   // e.target - gdzie event się wydaży - nav__link
+//   //e.currentTarget - element którego dotyczy obecne wydarzenie(albo ten, albo rodzic)
+//   console.log(e.currentTarget === this); //true
 
-  // Stop propagation - not a good idea
-  // e.stopPropagation();
-});
+//   // Stop propagation - not a good idea
+//   // e.stopPropagation();
+// });
 
-document.querySelector('.nav__links').addEventListener(
-  'click',
-  function (e) {
-    this.style.backgroundColor = randomColor();
-    console.log('CONTAINER', e.target, e.currentTarget); //nav__links,
-    // Stop propagation
-    // e.stopPropagation();
-  },
-  true
-);
+// document.querySelector('.nav__links').addEventListener(
+//   'click',
+//   function (e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log('CONTAINER', e.target, e.currentTarget); //nav__links,
+//     // Stop propagation
+//     // e.stopPropagation();
+//   },
+//   true
+// );
 
-document.querySelector('.nav').addEventListener('click', function (e) {
-  this.style.backgroundColor = randomColor();
-  console.log('NAV', e.target, e.currentTarget); //nav
-});
+// document.querySelector('.nav').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('NAV', e.target, e.currentTarget); //nav
+// });
